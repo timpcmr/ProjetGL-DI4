06793a761338e05501662c45707b86e192686367
Timothée Auffret <98650963+timpcmr@users.noreply.github.com>
2022-11-29 12:32:01 +0100

    Documentation et readme

---
+++ Documentation/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Projet Analyse d\'Images de Frelons'
+copyright = '2022, T. AUFFRET & L. SENLECQUE'
+author = 'T. AUFFRET & L. SENLECQUE'
+
+version = '1.0'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.napoleon',      # Supports Google / Numpy docstring 
+    'sphinx.ext.autodoc',       # Documentation from docstrings
+    'sphinx.ext.doctest',       # Test snippets in documentation
+    'sphinx.ext.todo',          # to-do syntax highlighting
+    'sphinx.ext.ifconfig',      # Content based configuration
+    'm2r2'                      # Markdown support 
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+source_suffix = ['.rst', '.md']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
